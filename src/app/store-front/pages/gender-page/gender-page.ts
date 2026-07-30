85fbddc3bd83891a6487630a008f5120bfe4368e
Francisco Schmidt <franschmidt2415@gmail.com>
2026-07-30 12:48:28 -0300

    Add per gender page

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products';
+import { map, of } from 'rxjs';
+import { ProductCard } from "@products/components/product-card/product-card";
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard, TitleCasePipe],
   templateUrl: './gender-page.html',
 })
-export class GenderPage {}
+export class GenderPage {
+  productsService = inject(ProductsService);
+  activatedRoute = inject(ActivatedRoute);
+  gender = toSignal(this.activatedRoute.params.pipe(
+    map(params => params['gender'])
+  ));
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => {
+      //if (!params.gender) return of([]);
+
+      return this.productsService.getProducts({ gender: params.gender });
+    }
+  });
+}
